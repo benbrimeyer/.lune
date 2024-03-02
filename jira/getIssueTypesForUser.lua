7fc0b653b5ceb0423cac2fb55f5f7bffe2ee0acc
Benjamin Brimeyer <bbrimeyer@roblox.com>
2024-03-02 14:44:36 -0800

    add todo function

---
+++ jira/getIssueTypesForUser.lua
@@ -0,0 +1,49 @@
+--!strict
+local process = require("@lune/process")
+local stdio = require("@lune/stdio")
+local net = require("@lune/net")
+local serde = require("@lune/serde")
+
+local JIRA_AUTH = require("../secrets/JIRA_AUTH")
+local JIRA_DOMAIN = require("../secrets/JIRA_DOMAIN")
+
+type IssueType = {
+    name: string,
+    id: string,
+    selfUrl: string,
+}
+local ISSUE_TYPES_WE_CARE_ABOUT = {
+    "Bug",
+    "Task",
+    "Story",
+}
+
+return function(): { IssueType }?
+    local jiraRequest = net.request({
+        url = `https://{JIRA_DOMAIN}.atlassian.net/rest/api/3/issuetype`,
+        method = "GET",
+        headers = {
+            Authorization = "Basic " .. JIRA_AUTH,
+        },
+    })
+
+    if jiraRequest.statusCode == 200 then
+        local response = serde.decode("json", jiraRequest.body)
+        local issueTypes = {}
+        for _, issueType in response do
+            if issueType.scope == nil then
+                if table.find(ISSUE_TYPES_WE_CARE_ABOUT, issueType.untranslatedName) ~= nil then
+                    table.insert(issueTypes,  {
+                        name = issueType.untranslatedName,
+                        id = issueType.id,
+                        selfUrl = issueType.self,
+                    })
+                end
+            end
+        end
+
+        return issueTypes
+    end
+
+    return nil
+end
---
+++ jira/getProjectFromKey.lua
@@ -0,0 +1,40 @@
+--!strict
+local process = require("@lune/process")
+local stdio = require("@lune/stdio")
+local net = require("@lune/net")
+local serde = require("@lune/serde")
+
+local JIRA_AUTH = require("../secrets/JIRA_AUTH")
+local JIRA_DOMAIN = require("../secrets/JIRA_DOMAIN")
+
+type Project = {
+    key: string,
+    id: string,
+    selfUrl: string,
+}
+
+return function(projectKey: string): Project?
+    local jiraRequest = net.request({
+        url = `https://{JIRA_DOMAIN}.atlassian.net/rest/api/3/project/search?keys={projectKey}`,
+        method = "GET",
+        headers = {
+            Authorization = "Basic " .. JIRA_AUTH,
+        },
+    })
+
+    if jiraRequest.statusCode == 200 then
+        local response = serde.decode("json", jiraRequest.body)
+        if response.total == 1 then
+            local projectResponse = response.values[1]
+            return {
+                key = projectResponse.key,
+                id = projectResponse.id,
+                selfUrl = projectResponse.self,
+            }
+        end
+
+        return nil
+    end
+
+    return nil
+end
